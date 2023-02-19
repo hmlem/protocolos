36a4e0911f2966fbe38367d1380b18cfa87704b1
Neto (João Batista) <bipnerds@gmail.com>
2023-02-19 01:19:55 -0300

    - Compondo classe de block - Dinamizando exemplo de blocos

---
+++ src/models/visualization/block.model.tsx
@@ -0,0 +1,117 @@
+import Action from "../data/action.model"
+import { Point } from "./point.model"
+
+export default class Block {
+
+    /** global */
+    hMargin = 50;
+    vMargin = 40;
+
+    /** Metadata */
+    id: number = -1
+    action?: Action = undefined
+
+    /** Size */
+    width?: number
+    height?: number
+
+    /** Text Box Size */
+    textBoxMaxWidth?: number
+    textBoxMaxHeight?: number
+
+    /** Center Point */
+    center?: Point
+
+    /** Connection Points */
+    top?: Point
+    left?: Point
+    right?: Point
+    bottom?: Point
+
+    /** Vertex Points */
+    topLeft?: Point
+    topRight?: Point
+    bottomLeft?: Point
+    bottomRight?: Point
+
+    /** Safebox */
+    safeboxCenter?: Point
+    safeboxWidth?: number
+    safeboxHeight?: number
+
+    blockStyle = { fill:'#9edb7d', stroke: '#478724', borderRadius:'30', rx: '10', ry:'10'};
+    safeboxStyle = { fillOpacity:'0.1', stroke: '#478724', strokeDasharray: '5,5' };
+    yellowPointStyle = { fill:'#fcf758', stroke: '#fc8458' };
+
+
+    constructor( centerPoint : Point,  ) {
+
+        //TODO:
+        // Define a largura e altura baseadas na quantidade de texto dentro da ação.
+        this.width = 350;
+        this.height = 400;
+
+        // TODO:
+        // Definido a partir do tamanho do texto.
+        // Dividir em sm, md, lg e xlg
+        this.textBoxMaxWidth = 300;
+        this.textBoxMaxHeight = 500;
+
+        this.center = centerPoint
+
+        this.topLeft = new Point(this.center.x - this.width / 2, this.center.y - this.height / 2 );
+
+        this.top = new Point( this.center.x, this.center.y - this.height/2 );
+        this.left = new Point( this.center.x - this.width/2, this.center.y );
+        this.right = new Point( this.center.x + this.width/2, this.center.y );
+        this.bottom = new Point( this.center.x, this.center.y + this.height/2 );
+
+        this.safeboxWidth = this.width + this.hMargin * 2;
+        this.safeboxHeight = this.height + this.vMargin*2;
+        this.safeboxCenter = new Point( this.center.x - this.width/2 - this.hMargin, this.center.y - this.height/2 - this.vMargin )
+
+    }
+
+    // TODO:  isRoot fica mais charmoso
+    isTheFirstBlock = () => this.id == 0
+
+    draw() {
+        return ( 
+            <g key={`block-${this.id}-text-container`}>
+                <rect key={`${this.id}`} x={this.topLeft!.x} y={this.topLeft!.y} width={this.width} height={this.height} style={this.blockStyle} />
+                <foreignObject key={`block-${this.id}-text`} x={this.topLeft!.x} y={this.topLeft!.y} width={this.width} height={this.height} >
+                    {this.insertText()}
+                </foreignObject>
+            </g>
+         )
+    }
+
+    insertText() {
+        // TODO: dar um jeito de pegar a altura para definir a altura do block a ser desenhado. se não o texto vai ser renderizado mas não vai dar pra ver pq 
+        // vai estar atrás do bloco 
+        return (
+            <div className="text-center mx-auto my-5" style={ {maxWidth: this.textBoxMaxWidth} }>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat 
+            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+        )
+    }
+
+    drawSafebox() {
+        return <rect key={`safebox-${this.id}`} x={this.safeboxCenter!.x} y={this.safeboxCenter!.y} width={this.safeboxWidth} height={this.safeboxHeight} style={this.safeboxStyle} />
+    }
+
+    drawConnectionPoints() {
+        return ( 
+        <g key={`connectection-points-${this.id}`}>
+            <circle key={`connectection-point-top-${this.id}`} cx={this.top!.x} cy={this.top!.y} r={5} style={this.yellowPointStyle} />
+            <circle key={`connectection-point-left-${this.id}`} cx={this.left!.x} cy={this.left!.y} r={5} style={this.yellowPointStyle} />
+            <circle key={`connectection-point-right-${this.id}`} cx={this.right!.x} cy={this.right!.y} r={5} style={this.yellowPointStyle} />
+            <circle key={`connectection-point-bottom-${this.id}`} cx={this.bottom!.x} cy={this.bottom!.y} r={5} style={this.yellowPointStyle} />
+        </g>
+        )
+    }
+
+}
--- src/pages/ExemplosBlocos.tsx
+++ src/pages/ExemplosBlocos.tsx
@@ -1,3 +1,4 @@
+import Block from "../models/visualization/block.model";
 import { Point } from "../models/visualization/point.model";
 
 export function ExemplosBlocos() {
@@ -6,52 +7,15 @@ export function ExemplosBlocos() {
     let flowchartWidth: number = 750;
     let flowchartHeight: number = 600;
 
-    // Espaçamento entre o bloco e a safebox
-    let hMargin = 50;
-    let vMargin = 40;
-
-    // A largura e altura do bloco serão definidas pela quantidade de texto
-    let blockWidth = 200;
-    let blockHeight = 80;
-
-    // O centro do bloco vai ser definido pela "nuvem de pontos"
-    let blockCenter = new Point(flowchartWidth/2, blockHeight/2 + vMargin );
-
-    // Ponto esquerdo superior
-    let blockX = blockCenter.x - blockWidth/2;
-    let blockY = blockCenter.y - blockHeight/2;
-
-    // Pontos de ligação do bloco
-    let pTop : Point = new Point( blockCenter.x, blockCenter.y - blockHeight/2 );
-    let pLeft : Point = new Point( blockCenter.x - blockWidth/2, blockCenter.y );
-    let pRight : Point = new Point( blockCenter.x + blockWidth/2, blockCenter.y );
-    let pBottom : Point = new Point( blockCenter.x, blockCenter.y + blockHeight/2 );
-
-    let safeboxWidth = blockWidth + hMargin*2;
-    let safeboxHeight = blockHeight + vMargin*2;
-
-    let safeboxX = blockCenter.x - blockWidth/2 - hMargin;
-    let safeboxY = blockCenter.y - blockHeight/2 - vMargin;
-
-    let blockStyle = { fill:'#9edb7d', stroke: '#478724', borderRadius:'30', rx: '10', ry:'10'};
-    let safeboxStyle = { fillOpacity:'0.1', stroke: '#478724', strokeDasharray: '5,5' };
-    let yellowPointStyle = { fill:'#fcf758', stroke: '#fc8458' };
+    let blocks = [new Block( new Point(flowchartWidth/2, 300 ) )];
 
     return ( 
     <div className="flex justify-center">
         
         <svg width={flowchartWidth} height={flowchartHeight} className="border-4 border-slate-600">
-
-        {/* Safebox */}
-        <rect x={safeboxX} y={safeboxY} width={safeboxWidth} height={safeboxHeight} style={safeboxStyle} />
-        {/* Block */}
-        <rect x={blockX} y={blockY} width={blockWidth} height={blockHeight} style={blockStyle} />
-        {/* Points */}
-        <circle cx={pTop.x} cy={pTop.y} r={5} style={yellowPointStyle} />
-        <circle cx={pLeft.x} cy={pLeft.y} r={5} style={yellowPointStyle} />
-        <circle cx={pRight.x} cy={pRight.y} r={5} style={yellowPointStyle} />
-        <circle cx={pBottom.x} cy={pBottom.y} r={5} style={yellowPointStyle} />
-
+            { blocks.map( block => block.draw() ) }
+            { blocks.map( block => block.drawSafebox() ) }
+            { blocks.map( block => block.drawConnectionPoints() ) }
         </svg>
 
     </div>
